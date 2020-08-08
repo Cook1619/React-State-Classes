d4b08adcdc9119415bec94a58c98f189200a0c72
Cook1619 <cook6348@gmail.com>
2020-08-07 21:17:32 -0500

    Adding conditional logic with state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import Random from "./RandomNumber";
 import BrokenButton from "./BrokenButton";
 
 // We're extending from the React.Component class
-class App extends Component {
-  constructor(props) {
-    // Super calls the constructor on React.Component
-    super(props);
-    this.state = {
-      score: 0,
-      gameOver: false,
-    };
-  }
-  //Never mutate state directly, don't set state in constructor or render
-  render() {
-    return (
-      <div>
-        <h1> Your score is: {this.state.score}</h1>
-        {/* <Random maxNumber={100} /> */}
-        <button>Click me</button>
-        <BrokenButton />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   constructor(props) {
+//     // Super calls the constructor on React.Component
+//     super(props);
+//     this.state = {
+//       score: 0,
+//       gameOver: false,
+//     };
+//   }
+//   //Never mutate state directly, don't set state in constructor or render
+//   render() {
+//     return (
+//       <div>
+//         {/* <h1> Your score is: {this.state.score}</h1> */}
+//         {/* <Random maxNumber={100} /> */}
+//         {/* <button>Click me</button>
+//         <BrokenButton /> */}
+//       </div>
+//     );
+//   }
+// }
 
 // class App extends Component {
 //   // Using this syntax without babel won't work, babel will insert the constructor and super for you
@@ -40,5 +40,30 @@ class App extends Component {
 //     );
 //   }
 // }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+      youWin: false,
+    };
+    this.handleNumber = this.handleNumber.bind(this);
+  }
+  handleNumber() {
+    this.setState({ number: this.state.number + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Your number is: {this.state.number}</h1>
+        {this.state.number <= 7 && (
+          <button onClick={this.handleNumber}>Click me</button>
+        )}
+        {this.state.number > 7 && <h3>YOU WIN</h3>}
+      </div>
+    );
+  }
+}
 
 export default App;
